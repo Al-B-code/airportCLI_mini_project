5179b2a9099899f2789df288623f3b6c3a96c227
Albert Hughes <albieh@me.com>
2023-10-24 11:42:18 +0100

    created flight skeleton and tests for corrosponding classes

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Flight {
+
+    ArrayList<Passenger> passengers;
+    String destination;
+    int uniqueIdentifier;
+
+    public Flight(String destination){
+        Random random = new Random();
+        this.destination = destination;
+        this.passengers = new ArrayList<>();
+        this.uniqueIdentifier = random.nextInt();
+    }
+
 }
