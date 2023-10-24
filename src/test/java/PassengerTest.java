9a0eb3f4ed9cda0f0b40dfee0543d6c798adc843
Albert Hughes <albieh@me.com>
2023-10-24 12:00:00 +0100

    changed phonenumber datatype to string in contactinfo, began to create a test for passenger.

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.BeforeEach;
+
 public class PassengerTest {
+
+    Passenger passenger;
+    ContactInfo contactInfo;
+    @BeforeEach
+    public void setUp(){
+        contactInfo = new ContactInfo("JohnSmith@email.com", "10 Downing Street", "07788229291");
+        passenger = new Passenger("John Smith", contactInfo);
+    }
+
 }
