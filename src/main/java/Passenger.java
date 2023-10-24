b94385e8164f6da7161db7238706cbe56f82061b
Sam Greenfield-Lee <samgreenfield-lee@Sams-MacBook-Air.local>
2023-10-24 11:34:14 +0100

    created passenger class with basic skeleton + created contact info class

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,2 +1,15 @@
+import java.util.Random;
+
 public class Passenger {
+    private String name;
+    ContactInfo contactInfo;
+    int uniqueId;
+
+
+    public Passenger (String name, ContactInfo contactInfo){
+        Random random = new Random();
+        this.name = name;
+        this.contactInfo = contactInfo;
+        this.uniqueId = random.nextInt();
+    }
 }
